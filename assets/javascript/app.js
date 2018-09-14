7d7f9500e1ddbac059847574b95882a7769a7b69
Efren Cavazos <efrencavazosimac@Efrens-iMac.attlocal.net>
2018-09-13 20:54:05 -0500

    updated questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,66 @@
 var startScreen;
 var gameHTML;
-var questionArray = ["Rap was just becoming well known in the 90s. Which artist had the first number one rap single?", "Novelty children shows were also a big hit. Which 90s TV show features characters with the name Tommy, Zack, Kimberly, Billy, and Trini?", "Which famous girl group of the 90s sung the theme song to the hit sketch comedy TV show 'All That'?", "Which record company had huge success in the 90s with artists such as the Notorious BIG, Mase, and Total?", "Teenybopper pop was big in the late 90s. Which teen pop idol was NOT in the 'Mickey Mouse Club'?", "'Clueless' was a cult hit in the 90s. Which cast member of 'Clueless' was NOT featured in the 'Clueless' TV series?", "What year was JonBenet Ramsey murdered?", "Which NBA team did NOT win a championship in the 90s?", "What group had the hit wonder in the 90s by the name of 'Lovefool'?", "These two best friends came together to write the Oscar winning movie, 'Good Will Hunting'?"];
-var answerArray = [["Kriss Kross", "MC Hammer", "Vanilla Ice", "DJ Jazzy Jeff and the Fresh Prince"], ["Saved By the Bell", "The Mighty Morphin Power Rangers", "Beverly Hills 90210", "All That"], ["TLC", "Envogue", "Destiny's Child", "3LW"], ["Motown", "Death Row", "Murder Inc.", "Bad Boy"], ["Jessica Simpson", "JC Chasez", "Britney Spears", "Justin Timberlake"], ["Stacey Dash", "Elisa Donovan", "Donald Faison", "Alicia Silverstone"], ["1995", "1996", "1998", "1997"], ["Los Angeles Lakers", "Detroit Pistons", "Houston Rockets", "San Antonio Spurs"], ["The Corrs", "Chumbawumba", "Was Not Was", "The Cardigans"], ["Ben Stiller and Owen Wilson", "Ben Affleck and Matt Damon", "Jim Carey and Will Smith", "Demi Moore and Bruce Willis"]];
-var correctAnswersArray = ["C. Vanilla Ice <br/> There were many popular rap hit throughout the early nineties.", "B. The Mighty Morphin Power Rangers <br/> Tommy was the green and white ranger, Zack was the black ranger, Kimberly was the pink ranger, Billy was the blue ranger, and Trini (which later became Alicia) was the yellow ranger.", "A. TLC <br/> The theme song stayed with the show until the 2001-present season.", "D. Bad Boy <br/> Bad Boy records was founded by Sean 'Puffy/P.Diddy/Puff Daddy' Combs. Other Bad Boy artists included Lil' Kim, Junior Mafia, Harlem World, Craig Mack, 112, Faith Evans, Carl Thomas, Loon, and the Lox.", "A. Jessica Simpson <br/> Jessica Simpson tried out for the 'Mickey Mouse Club' and had good chances of being chosen but during her audition, she had stage fright.", "D. Alicia Silverstone <br/> Alicia Silverstone did not decide to join the cast of the 'Clueless' TV series but instead, Rachael Blanchard. Alicia Silverstone went on to star in 'Excess Baggage' and 'Batman and Robin'.", "D. 1997 <br/> JonBenet was mysteriously murdered in her home in late December. Fingers were pointed to her parents but as of 2004, her murder has not been solved.", "B. Detroit Pistons <br/> The Detroit Pistons won in 1990. The Houston Rockets won in 1994 and 1995. The San Antonio Spurs won in 1999. All the other championships won in the 90s were by the Chicago Bulls.", "The Cardigans <br/> 'Lovefool' was featured in the hit movie 'Romeo & Juliet' starring Leonardo DiCaprio and Claire Danes.", "Ben Affleck and Matt Damon <br/> Ben Affleck and MAtt Damon also starred in 'Good Will Hunting'", "'Screw you guys...I'/m going home. <br/> When Kyle plays with his little brother Ike, they play a game called Kick the Baby.", "28 Days <br/> '28 Days' starred Sandra Bullock", "Whitney Houston <br/> Whitney Houston'/s edition of the 'Star Spangled Banner' is one of the most popular renditions of the song.", "The Little Mermaid <br/> 'The Little Mermaid' debuted in 1989.", "Billy Ray Cyrus <br/> Billy Ray Cyrus had great success with his song 'Achy Breaky Heart.' He starred in the PAX TV series 'Doc'."];
-var imageArray = ["<img class='center-block' src='assets/images/vanilla-ice.jpg'>","<img class='center-block' src='assets/images/The-Rangers-mighty-morphin-power-rangers.jpg'>","<img class='center-block' src='assets/images/TLC.jpg'>","<img class='center-block' src='assets/images/bad-boy-records.jpg'>","<img class='center-block' src='assets/images/Jessica-simpson.jpg'>","<img class='center-block' src='assets/images/alicia-silverstone.jpg'>","<img class='center-block' src='assets/images/Jonbenet.jpg'>","<img class='center-block' src='assets/images/Detroit-Pistons-Logo.jpg'>","<img class='center-block' src='assets/images/the-cardigans.jpg'>","<img class='center-block' src='assets/images/ben-affleck-matt-damon.jpg'>","<img class='center-block' src='assets/images/south-park.jpg'>","<img class='center-block' src='assets/images/28-days-later.jpg'>","<img class='center-block' src='assets/images/whitneyhouston.jpg'>","<img class='center-block' src='assets/images/the-little-mermaid.jpg'>","<img class='center-block' src='assets/images/billy-ray-cyrus.jpg'>"];
+var questionArray = ["<span class='text-black text-large padding-10px-bottom'><strong>Question 1</strong></span><br/><p class='text-medium text-center'>Rap was just becoming well known in the 90s.<br/> Which artist had the first number one rap single?</p>", 
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 2</strong></span><br/><p class='text-medium text-center'>Novelty children shows were also a big hit. Which 90s TV show features characters with the name Tommy, Zack, Kimberly, Billy, and Trini?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 3</strong></span><br/><p class='text-medium text-center'>Which famous girl group of the 90s sung the theme song to the hit sketch comedy TV show 'All That'?</p>", 
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 4</strong></span><br/><p class='text-medium text-center'>Which record company had huge success in the 90s with artists such as the Notorious BIG, Mase, and Total?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 5</strong></span><br/><p class='text-medium text-center'>Teenybopper pop was big in the late 90s. Which teen pop idol was NOT in the 'Mickey Mouse Club'?</p>", 
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 6</strong></span><br/><p class='text-medium text-center'>'Clueless' was a cult hit in the 90s. Which cast member of 'Clueless' was NOT featured in the 'Clueless' TV series?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 7</strong></span><br/><p class='text-medium text-center'>What year was JonBenet Ramsey murdered?", "Which NBA team did NOT win a championship in the 90s?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 8</strong></span><br/><p class='text-medium text-center'>Which NBA team did NOT win a championship in the 90s?</p>",  
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 9</strong></span><br/><p class='text-medium text-center'>What group had the hit wonder in the 90s<br/>by the name of 'Lovefool'?</p>", 
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 10</strong></span><br/><p class='text-medium text-center'>These two best friends came together to write the Oscar winning movie, 'Good Will Hunting'?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 11</strong></span><br/><p class='text-medium text-center'>Which phrase is a common catchphrase on 'South Park'?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 12</strong></span><br/><p class='text-medium text-center'>Which movie did NOT star actor Brad Pitt?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 13</strong></span><br/><p class='text-medium text-center'>Who sang the 'Star Spangled Banner' at the Super Bowl XXV?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 14</strong></span><br/><p class='text-medium text-center'>The 1990s was a golden age for modern Disney movies. Which Disney Movie did NOT debut in the 90s?</p>",
+                "<span class='text-black text-large padding-10px-bottom'><strong>Question 15</strong></span><br/><p class='text-medium text-center'>Country Music became very big in the 90s. Which country music star had a popular television show on 'PAX TV'?</p>"];
+
+var answerArray = [["Kriss Kross", "MC Hammer", "Vanilla Ice", "DJ Jazzy Jeff and the Fresh Prince"], 
+                ["Saved By the Bell", "The Mighty Morphin Power Rangers", "Beverly Hills 90210", "All That"], 
+                ["TLC", "Envogue", "Destiny's Child", "3LW"], ["Motown", "Death Row", "Murder Inc.", "Bad Boy"], 
+                ["Jessica Simpson", "JC Chasez", "Britney Spears", "Justin Timberlake"], 
+                ["Stacey Dash", "Elisa Donovan", "Donald Faison", "Alicia Silverstone"], 
+                ["1995", "1996", "1998", "1997"], ["Los Angeles Lakers", "Detroit Pistons", "Houston Rockets", "San Antonio Spurs"], 
+                ["The Corrs", "Chumbawumba", "Was Not Was", "The Cardigans"], 
+                ["Ben Stiller and Owen Wilson", "Ben Affleck and Matt Damon", "Jim Carey and Will Smith", "Demi Moore and Bruce Willis"],
+                ["'Screw you guys...I'/m going home.'", "You are dead ducks boys!", "Oh no silly gooses!", "Kick it and kick it good."],
+                ["28 Days", "Seven Years in Tibet", "Se7en", "Twelve Monkeys"],
+                ["Celine Dion", "Toni Braxton", "Mariah Carey", "Whitney Houston"],
+                ["The Little Mermaid", "The Lion King", "Beauty and the Beast","Tarzan"],
+                ["Shania Twain", "Billy Ray Cyrus", "Garth Brooks", "Faith Hill"]];
+
+var correctAnswersArray = ["<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>C. Vanilla Ice</strong></h3><br/><p class='text-center'>There were many popular rap hit throughout the early nineties.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>B. The Mighty Morphin Power Rangers</strong><br/><p class='text-center'>Tommy was the green and white ranger, Zack was the black ranger, Kimberly was the pink ranger, Billy was the blue ranger, and Trini (which later became Alicia) was the yellow ranger.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>A. TLC</strong><br/><p class='text-center'>The theme song stayed with the show until the 2001-present season.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>D. Bad Boy</strong><br/><p class='text-center'>Bad Boy records was founded by Sean 'Puffy/P.Diddy/Puff Daddy' Combs. Other Bad Boy artists included Lil' Kim, Junior Mafia, Harlem World, Craig Mack, 112, Faith Evans, Carl Thomas, Loon, and the Lox.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>A. Jessica Simpson</strong><br/><p class='text-center'>Jessica Simpson tried out for the 'Mickey Mouse Club' and had good chances of being chosen but during her audition, she had stage fright.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>D. Alicia Silverstone</strong><br/><p class='text-center'>Alicia Silverstone did not decide to join the cast of the 'Clueless' TV series but instead, Rachael Blanchard. Alicia Silverstone went on to star in 'Excess Baggage' and 'Batman and Robin'</p>.", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>D. 1997</strong><br/><p class='text-center'>JonBenet was mysteriously murdered in her home in late December. Fingers were pointed to her parents but as of 2004, her murder has not been solved.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>B. Detroit Pistons</strong><br/><p class='text-center'>The Detroit Pistons won in 1990. The Houston Rockets won in 1994 and 1995. The San Antonio Spurs won in 1999. All the other championships won in the 90s were by the Chicago Bulls.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>A. The Cardigans</strong><br/><p class='text-center'>'Lovefool' was featured in the hit movie 'Romeo & Juliet' starring Leonardo DiCaprio and Claire Danes.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>D. Ben Affleck and Matt Damon</strong><br/><p class='text-center'>Ben Affleck and MAtt Damon also starred in 'Good Will Hunting'.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>A. 'Screw you guys...I'/m going home.</strong><br/><p class='text-center'>When Kyle plays with his little brother Ike, they play a game called Kick the Baby.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>A. 28 Days</strong><br/><p class='text-center'>'28 Days' starred Sandra Bullock.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>D. Whitney Houston</strong><br/><p class='text-center'>Whitney Houston'/s edition of the 'Star Spangled Banner' is one of the most popular renditions of the song.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>A. The Little Mermaid</strong><br/><p class='text-center'>'The Little Mermaid' debuted in 1989.</p>", 
+                "<h3 class='text-black text-center padding-10px-bottom padding-10px-top'><strong>B. Billy Ray Cyrus</strong><br/><p class='text-center'>Billy Ray Cyrus had great success with his song 'Achy Breaky Heart.' He starred in the PAX TV series 'Doc'.</p>"];
+
+var imageArray = ["<img class='center-block' src='assets/images/vanilla-ice.jpg'>",
+                "<img class='center-block' src='assets/images/The-Rangers-mighty-morphin-power-rangers.jpg'>",
+                "<img class='center-block' src='assets/images/TLC.jpg'>",
+                "<img class='center-block' src='assets/images/bad-boy-records.jpg'>",
+                "<img class='center-block' src='assets/images/Jessica-simpson.jpg'>",
+                "<img class='center-block' src='assets/images/alicia-silverstone.jpg'>",
+                "<img class='center-block' src='assets/images/Jonbenet.jpg'>",
+                "<img class='center-block' src='assets/images/Detroit-Pistons-Logo.jpg'>",
+                "<img class='center-block' src='assets/images/the-cardigans.jpg'>",
+                "<img class='center-block' src='assets/images/ben-affleck-matt-damon.jpg'>",
+                "<img class='center-block' src='assets/images/south-park.jpg'>",
+                "<img class='center-block' src='assets/images/28-days-later.jpg'>",
+                "<img class='center-block' src='assets/images/whitneyhouston.jpg'>",
+                "<img class='center-block' src='assets/images/the-little-mermaid.jpg'>",
+                "<img class='center-block' src='assets/images/billy-ray-cyrus.jpg'>"];
 var questionCounter = 0;
 var counter = 30;
 var selectAnswer;
@@ -65,15 +122,15 @@ $(document).ready(function () {
 
 function generateLossDueToTimeOut() {
     unansweredAnswer++;
-    gameHTML = "<div class='logo'><p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
-        counter + "</span></p>" + "<p class='text-center'>You ran out of time!<br />  The correct answer was:<br />" +
+    gameHTML = "<p class='logo'><p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        counter + "</span></p>" + "<p class='text-center text-large text-white'><strong>You ran out of time!</strong><br /> The correct answer was:<br /> " +
         correctAnswersArray[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/x.png'>";
     $(".mainArea").html(gameHTML);
     setTimeout(wait, 4000);
 }
 function generateWin() {
     correctAnswer++;
-    gameHTML = "<div class='logo'><p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+    gameHTML = "<p class='logo'><p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
         counter + "</span></p>" + "<p class='text-center'>Correct!<br />  The answer is:</br> " +
         correctAnswersArray[questionCounter] + "</p>" + imageArray[questionCounter];
     $(".mainArea").html(gameHTML);
@@ -82,14 +139,21 @@ function generateWin() {
 // Wrong answer
 function generateLoss() {
     incorrectAnswer++;
-    gameHTML = "<div class='logo'><p class='text-center timer-p'>Time Remaining:<br/> <span class='timer'>" +
-        counter + "</span></p>" + "<p class='text-center'><span class='text-large text-orange'>Wrong!</span><br /> The correct answer is: " +
+    gameHTML = "<p class='logo'><h3 class='text-center timer-p text-medium'>Time Remaining: <span class='timer'>" +
+        counter + "</span></h3>" + "<p class='text-center'><span class='text-large text-black'><strong>Wrong!</strong></span><br /> The correct answer is: " +
         correctAnswersArray[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/x.png'>";
+        
     $(".mainArea").html(gameHTML);
-    setTimeout(wait, 4000);
+    setTimeout(wait, 1000);
 }
 function generateHTML() {
-    gameHTML = "<div class='logo'><p class='logo'><p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. " + answerArray[questionCounter][1] + "</p><p class='answer'>C. " + answerArray[questionCounter][2] + "</p><p class='answer'>D. " + answerArray[questionCounter][3] + "</p>";
+    gameHTML = "<p class='logo'><p class='text-center timer-p'><span class='text-large text-white'><strong>Time Remaining: <span class='timer text-black'>30</strong></span></p><p class='text-center'>" + 
+    questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + 
+    answerArray[questionCounter][0] + "</p><p class='answer'>B. " + 
+    answerArray[questionCounter][1] + "</p><p class='answer'>C. " + 
+    answerArray[questionCounter][2] + "</p><p class='answer'>D. " + 
+    answerArray[questionCounter][3] + "</p>";
+
     $(".mainArea").html(gameHTML);
 }
 function wait() {
